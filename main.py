da1a75aabeb9690446de212094170b9e8e638066
Taro Morita <morita0079@gmail.com>
2022-10-08 01:36:51 +0900

    fix main

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ from bs4 import BeautifulSoup
 import slackweb
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
-slack = slackweb.Slack(
-    url=os.environ["SLACK_WEBHOOK_URL"]
-)
 
-url = 'https://kinesis-ergo.com/keyboards/advantage360/'
-res = requests.get(url)
-soup = BeautifulSoup(res.text, 'html.parser')
-element = soup.select(
-    '#main > div.fg-inner-page-nav.tk-proxima-nova > div > div > div > div > a.fg-inner-page-nav__list-item.fg-inner-page-nav__list-item--alt-color'
-)
+def main():
 
-if element[0].text == 'Sold out':
-    slack.notify(text='Advantage 360 is sold out now: ' + url)
-else:
-    slack.notify(
-        text='💖💖💖💖 Advantage 360 is maybe available now !! check it out: ' + url + ' 💖💖💖💖')
+    load_dotenv()
+
+    slack = slackweb.Slack(
+        url=os.environ["SLACK_WEBHOOK_URL"]
+    )
+
+    url = 'https://kinesis-ergo.com/keyboards/advantage360/'
+    res = requests.get(url)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    element = soup.select(
+        '#main > div.fg-inner-page-nav.tk-proxima-nova > div > div > div > div > a.fg-inner-page-nav__list-item.fg-inner-page-nav__list-item--alt-color'
+    )
+
+    if element[0].text == 'Sold out':
+        slack.notify(text='Advantage 360 is sold out now: ' + url)
+    else:
+        slack.notify(
+            text='💖💖💖💖 Advantage 360 is maybe available now !! check it out: ' + url + ' 💖💖💖💖')
+
+
+if __name__ == '__main__':
+    main()
